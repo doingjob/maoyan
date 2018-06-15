cef0aa39abec7ceb7951aa7effc169e91270b195
doingjob <how_do_u_do@outlook.com>
2018-06-15 12:03:18 +0800

    finally

--- spider.py
+++ spider.py
@@ -1,10 +1,13 @@
 import requests
 from requests.exceptions import RequestException
 import re
+import json
+
+
 def get_one_page(url):
     headers={
             "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17" 
-            }
+    }
     try:
         response=requests.get(url,headers=headers,timeout=2)
         if response.status_code==200:
@@ -18,16 +21,31 @@ def parse_one_page(html):
                          +'.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
                          +'.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>',re.S)
     items=re.findall(pattern,html)
-    return items
+    for item in items:
+        yield{
+                'index':item[0],
+                'image':item[1],
+                'title':item[2],
+                'actors':item[3].strip()[3:],
+                'time':item[4].strip()[5:],
+                'score':item[5]+item[6]       
+        }
+
+def write_to_file(content):
+    with open('result.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(content,ensure_ascii=False) +'\n')
+        f.close()
 
 
 
-def main():
-    url="http://maoyan.com/board/4"
+
+def main(offset):
+    url="http://maoyan.com/board/4?offset="+str(offset)
     html=get_one_page(url)
-    items=parse_one_page(html)
+    for item in parse_one_page(html):
+        write_to_file(item)
 
-    print(items)
 
 if __name__=="__main__":
-    main()
+    for i in range(10):
+        main(i*10)
